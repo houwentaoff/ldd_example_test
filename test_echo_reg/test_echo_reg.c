53fbd349501466e8261579b6b79c9368e5f11520
housir <houwentaoff@gmail.com>
2016-08-14 16:58:10 +0800

    [FEA] 增加platform的测试例子.

--- test_echo_reg/test_echo_reg.c
+++ test_echo_reg/test_echo_reg.c
@@ -42,10 +42,10 @@ static ssize_t reg_read(struct file *filp, char *buffer, size_t count, loff_t *p
 static ssize_t reg_write(struct file *file,const char __user *buf, size_t count, loff_t *ppos);
 
 static struct file_operations reg_read_ops = {
-      owner:THIS_MODULE,
-      open:reg_open,
-      read:reg_read,
-      write: reg_write, 
+      .owner   =  THIS_MODULE,
+      .open    =  reg_open,
+      .read    =  reg_read,
+      .write   =  reg_write, 
 };
 
 static ssize_t reg_write(struct file *file,const char __user *buf, size_t count, loff_t *ppos)
@@ -64,11 +64,11 @@ static ssize_t reg_write(struct file *file,const char __user *buf, size_t count,
     virt_addr = (unsigned long *)ioremap(reg_addr, sizeof(unsigned long));
     if (!virt_addr) {
         printk("Unable to map\n");
-		rc = -ENOMEM;
-		goto err_reg_map;
-	}
+        rc = -ENOMEM;
+        goto err_reg_map;
+    }
     printk("phy addr[0x%x], value[0x%x]\n", reg_addr, *virt_addr);
-	iounmap((void *)virt_addr);     
+    iounmap((void *)virt_addr);     
 err_reg_map:
     return count;
 }
@@ -95,43 +95,43 @@ static int reg_init(void)
     dev_t devid;
     result = (int) apple;
 
- printk("===> %s\n", __func__);
-	/* 初始化字符设备 */
-	if (major)
+    printk("===> %s\n", __func__);
+    /* 初始化字符设备 */
+    if (major)
     {
         devid = MKDEV(major,0);/* 主设备号是major，次设备号是0 */
-		result = register_chrdev_region(devid, 32, DEVNAME);
+        result = register_chrdev_region(devid, 32, DEVNAME);
     }
     else 
     {
-		result = alloc_chrdev_region(&devid, 0, 32, DEVNAME);
-		major = MAJOR(devid);
-	}
+        result = alloc_chrdev_region(&devid, 0, 32, DEVNAME);
+        major = MAJOR(devid);
+    }
 
 #if 1
-	if (result < 0) {
-//		dev_err(&pdev->dev, "register-chrdev failed: %d\n", result);
+    if (result < 0) {
+//        dev_err(&pdev->dev, "register-chrdev failed: %d\n", result);
         printk("register-chrdev failed: %d\n", result);
-		goto undo_request_region;
-	}
-	if (!major) {
-		major = result;
-//		dev_dbg(&pdev->dev, "got dynamic major %d\n", major);
+        goto undo_request_region;
+    }
+    if (!major) {
+        major = result;
+//        dev_dbg(&pdev->dev, "got dynamic major %d\n", major);
         printk("got dynamic major %d\n", major);
-	}
+    }
 #endif
 
-	cdev_init(&reg_read_cdev, &reg_read_ops);//初始化结构体struct cdev
-//	reg_read_cdev.owner = THIS_MODULE;
+    cdev_init(&reg_read_cdev, &reg_read_ops);//初始化结构体struct cdev
+//    reg_read_cdev.owner = THIS_MODULE;
 
     cdev_add (&reg_read_cdev, devid, 32);//将结构提struct cdev添加到系统之中
 
-	housir_class = class_create(THIS_MODULE, "housir_class");
-	if (IS_ERR(housir_class)) {
-		printk(KERN_ERR "class_create() failed for housir_class\n");
-		result = PTR_ERR(housir_class);
-		goto out_chrdev;
-	}
+    housir_class = class_create(THIS_MODULE, "housir_class");
+    if (IS_ERR(housir_class)) {
+        printk(KERN_ERR "class_create() failed for housir_class\n");
+        result = PTR_ERR(housir_class);
+        goto out_chrdev;
+    }
 
     device_create(housir_class, NULL, MKDEV(major, 0), NULL,
                   "housir_major%d", 0);    
@@ -141,22 +141,22 @@ static int reg_init(void)
 
 out_chrdev:
     printk("housir_init::register failure!!\n");
-	unregister_chrdev_region(MKDEV(major, 0), 32);//卸载设备驱动所占有的资源
+    unregister_chrdev_region(MKDEV(major, 0), 32);//卸载设备驱动所占有的资源
 undo_request_region:
-//	release_region(pc8736x_gpio_base, PC8736X_GPIO_RANGE);
+//    release_region(pc8736x_gpio_base, PC8736X_GPIO_RANGE);
 
-	return result;
+    return result;
 }
 static  void reg_exit(void)
 {
     printk("===> %s\n", __func__);
 
     device_destroy(housir_class, MKDEV(major, 0));
-	if (housir_class)
-		class_destroy(housir_class);
+    if (housir_class)
+        class_destroy(housir_class);
     cdev_del(&reg_read_cdev);
     if (major)
-	    unregister_chrdev_region(MKDEV(major, 0), 32);//卸载设备驱动所占有的资源
+        unregister_chrdev_region(MKDEV(major, 0), 32);//卸载设备驱动所占有的资源
    printk(KERN_INFO"demo exit\n");
    printk("<=== %s\n", __func__);
 }
---
+++ test_platform/test_platform1.c
@@ -0,0 +1,154 @@
+/*
+ * =====================================================================================
+ *       Copyright (c), 2013-2020, xxx.
+ *       Filename:  test_platform1.c
+ *
+ *    Description:  测试在bsp中和在当前驱动中直接注册platform device
+ *                  2中platform方法
+ *         Others:  相关的api:
+ *                  platform_driver_register platform_driver_unregister
+ *                  platform_get_drvdata platform_set_drvdata
+ *
+ *        Version:  1.0
+ *        Created:  Sunday, August 14, 2016 10:48:35 CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Joy. Hou (hwt), 544088192@qq.com
+ *   Organization:  xxx
+ *
+ * =====================================================================================
+ */
+#if 0
+
+/*-----------------------------------------------------------------------------
+ *  bsp中的定义如下
+ *-----------------------------------------------------------------------------*/
+static struct platform_device *smdkc210_devices[] __initdata = {
+    &s3c_device_hsmmc0,
+    &s3c_device_hsmmc1,
+    &s3c_device_hsmmc2,
+    &s3c_device_hsmmc3,
+    &s3c_device_i2c1,
+    &s3c_device_rtc,
+    &s3c_device_wdt,
+    &exynos4_device_ac97,
+    &exynos4_device_i2s0,
+    &exynos4_device_pd[PD_MFC],
+    &exynos4_device_pd[PD_G3D],
+    &exynos4_device_pd[PD_LCD0],
+    &exynos4_device_pd[PD_LCD1],
+    &exynos4_device_pd[PD_CAM],
+    &exynos4_device_pd[PD_TV],
+    &exynos4_device_pd[PD_GPS],
+    &exynos4_device_sysmmu,
+    &samsung_asoc_dma,
+    &smdkc210_smsc911x,
+};
+/*  */
+platform_add_devices(smdkc210_devices, ARRAY_SIZE(smdkc210_devices));
+#endif
+
+#include <linux/string.h>
+#include <linux/sysfs.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/device.h>
+#include <linux/io.h>
+#include <linux/platform_device.h>
+#include <linux/slab.h>
+
+struct test_data {
+    const char * const str;
+};
+struct test_data test_platform_data = {
+    .str  = "this is test platform data!"
+};
+static void platform_test_release(struct device * dev)
+{
+    return ;
+}
+struct platform_device test_device = {
+    .name  = "test platform",
+    .dev   = {
+        .release       = platform_test_release,
+        .platform_data = &test_platform_data,
+    }
+};
+
+static int dm9000_drv_remove(struct platform_device *pdev)
+{
+    struct test_data *pdata = pdev->dev.platform_data;
+    char *rbuff = platform_get_drvdata(pdev);
+
+    printk("==>%s data[%s]\n", __func__, pdata->str);
+    
+    printk("<==%s rbuff[%s]\n", __func__, rbuff);
+    if (rbuff) {
+        kfree(rbuff);
+    }
+    return 0;
+}
+static int dm90001_probe(struct platform_device *pdev)
+{
+    int ret = 0;
+    struct test_data *pdata = pdev->dev.platform_data;
+    char * rbuff = NULL;
+
+    printk("==>%s data[%s]\n", __func__, pdata->str);
+
+    rbuff = kmalloc(100, GFP_KERNEL);
+    if (rbuff == NULL ) {
+        ret = -ENOBUFS;
+        goto out_free;
+    }
+    platform_set_drvdata(pdev, rbuff);
+    strcpy(rbuff, "just test set/get drv data!!");
+
+    printk("<==%s data[%s]rbuff[%s]\n", __func__, pdata->str,
+            rbuff);
+
+    return ret;
+out_free:
+    kfree(rbuff);    
+    return ret;
+}
+static struct platform_driver test_platform_driver = {
+    .driver= {
+        .name  = "test platform",
+        .owner = THIS_MODULE,
+//        .pm    = &dm9000_drv_pm_ops,
+    },
+    .probe   = dm90001_probe,
+    .remove  = dm9000_drv_remove,
+};
+static int test_platform_device_init(void)
+{
+    return platform_device_register(&test_device);
+}
+static void test_platform_device_exit(void)
+{
+    platform_device_unregister(&test_device);
+}
+static int __init test_platform_init(void)
+{
+    printk(KERN_INFO "==>%s init\n", __func__);
+    test_platform_device_init();
+    return platform_driver_register(&test_platform_driver);
+}
+
+static void __exit test_platform_exit(void)
+{
+    test_platform_device_exit();
+    platform_driver_unregister(&test_platform_driver);
+    printk(KERN_INFO "<==%s exit\n", __func__);
+}
+
+module_init(test_platform_init);
+module_exit(test_platform_exit);
+MODULE_AUTHOR("Joy.Hou China Chengdu");
+MODULE_DESCRIPTION("test platform driver");
+MODULE_LICENSE("GPL");
