500e42567cd401fd8afeec11d59f90a8826dba80
Joy Hou <544088192@qq.com>
2017-02-19 16:36:53 +0800

    [STY] Add task_hi.

--- test_tasklet_hi/test_tasklet_hi.c
+++ test_tasklet_hi/test_tasklet_hi.c
@@ -1,15 +1,15 @@
 /*
  * =====================================================================================
  *       Copyright (c), 2013-2020, Jz.
- *       Filename:  test_tasklet.c
+ *       Filename:  test_tasklet_hi.c
  *
  *    Description:  
  *         Others: 1. 尽可能的快的执行小任务，执行的开始时间不定，在任务执行的函数中不允许使用带阻塞
  *         的函数
- *                 2. 经过测试发现时间差为2us到1ms之间，在几十次测试中1ms只出现一次；大部
- *                 分落在10us以内.
  *                 3. 下半部运行时是允许中断请求的，而上半部运行时是关中断的, tasklet会被
  *                 中断打断。
+ *                 4. 这个在声卡和网卡中常使用，在CPU的tasklet_hi_vec中排队
+ *                 5. 使用HI_SOFTIRQ作为软中断,而不是TASKLET_SOFTIRQ
  *
  *        Version:  1.0
  *        Created:  08/09/2016 11:05:23 AM
@@ -45,7 +45,7 @@ static struct cdev reg_read_cdev;
 volatile unsigned long regs_addr = 0;
 volatile unsigned long *virt_addr = 0;
 
-static struct tasklet_struct my_tasklet;
+static struct tasklet_struct tasklet_capture;
 
 static int reg_open(struct inode *inode, struct file *filp);
 static ssize_t reg_read(struct file *filp, char *buffer, size_t count, loff_t *ppos);
@@ -59,16 +59,17 @@ static struct file_operations reg_read_ops = {
 };
 static void tasklet_handler (unsigned long data)
 {
-    struct timeval tv;
-    do_gettimeofday(&tv);
-    printk("tasklet_handler is running. s:%ld, us:%ld\n", tv.tv_sec, tv.tv_usec);
+    struct timeval time;
+    do_gettimeofday(&time);
+    printk("hi tasklet_handler is running.s:%ld us:%ld\n", time.tv_sec, time.tv_usec);
 }
 static ssize_t reg_write(struct file *file,const char __user *buf, size_t count, loff_t *ppos)
 {
-    struct timeval tv;
-    do_gettimeofday(&tv);
-    tasklet_schedule(&my_tasklet);
-    printk("tasklet schedule !!! s:%ld. us:%ld\n", tv.tv_sec, tv.tv_usec);
+    struct timeval time;
+
+    tasklet_hi_schedule(&tasklet_capture);
+    do_gettimeofday(&time);
+    printk("hi tasklet schedule !!! s:%ld us:%ld \n", time.tv_sec, time.tv_usec);
     return count;
 }
 
@@ -98,7 +99,7 @@ static int reg_init(void)
     result = (int) apple;
 
     printk("===> %s\n", __func__);
-    tasklet_init(&my_tasklet,tasklet_handler,0);
+    tasklet_init(&tasklet_capture,tasklet_handler,0);
     printk("init tasklet!!!\n");
     /* 初始化字符设备 */
     if (major)
@@ -157,7 +158,7 @@ static  void reg_exit(void)
     cdev_del(&reg_read_cdev);
     if (major)
         unregister_chrdev_region(MKDEV(major, 0), 32);//卸载设备驱动所占有的资源
-    tasklet_kill(&my_tasklet);
+    tasklet_kill(&tasklet_capture);
     printk(KERN_INFO"demo exit\n");
     printk("<=== %s\n", __func__);
 }
